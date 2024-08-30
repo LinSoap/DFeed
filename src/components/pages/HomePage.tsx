a9a5395a5dfad8f70ecaebd0c1f3080c9c23b452
LinSoap <838819583@qq.com>
2024-08-30 22:11:40 +0800

    feat:add import and export opml

--- src/components/pages/HomePage.tsx
+++ src/components/pages/HomePage.tsx
@@ -1,5 +1,42 @@
+import { Button, Input } from "@chakra-ui/react";
+import { useState } from "react";
+import { useKubo } from "../providers/KuboProvider";
+import { catFileFromPath, Uint8ArrayToText } from "../../utils/kubo";
+import { downloadFile } from "../../utils/download";
+
 const HomePage = () => {
-  return <div>HomePage</div>;
+  const [file, setFile] = useState<File | null>(null);
+  const [filePath, setFilePath] = useState<string | null>(null);
+  const { kuboClient } = useKubo();
+  console.log(file);
+
+  const handleImport = async () => {
+    console.log(kuboClient);
+    if (!file) return;
+    const res = await kuboClient?.add(file);
+    setFilePath(res.path);
+    console.log(res);
+  };
+
+  const handleExport = async () => {
+    if (!filePath) return;
+    const res = await catFileFromPath(filePath, kuboClient);
+    const text = Uint8ArrayToText(res);
+    console.log(text);
+    downloadFile(res, "header.opml");
+  };
+
+  return (
+    <>
+      <Input
+        type="file"
+        onChange={(e) => setFile(e.target.files ? e.target.files[0] : null)}
+      />
+      <p>{filePath}</p>
+      <Button onClick={handleImport}>Import</Button>
+      <Button onClick={handleExport}>Export</Button>
+    </>
+  );
 };
 
 export default HomePage;
---
+++ src/utils/kubo.ts
@@ -0,0 +1,28 @@
+import { KuboRPCClient } from "kubo-rpc-client";
+
+export async function catFileFromPath(path: string, kubo: KuboRPCClient) {
+  const chunks = []; 
+    for await (const chunk of kubo.cat(path)) {
+    chunks.push(chunk); 
+  }
+
+  const fullData = new Uint8Array(chunks.reduce((acc, curr) => acc + curr.length, 0));
+  let offset = 0;
+  for (const chunk of chunks) {
+    fullData.set(chunk, offset);
+    offset += chunk.length;
+  }
+
+  return fullData;
+}
+
+
+export function Uint8ArrayToText(uint8Array: Uint8Array) {
+  return new TextDecoder().decode(uint8Array);
+}
+
+export function TextToUint8Array(text:string) {
+    const encoder = new TextEncoder();
+    return encoder.encode(text);
+}
+
