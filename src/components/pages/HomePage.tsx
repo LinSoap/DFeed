797be9f555041199400c62bb4e77ad97e6be4504
LinSoap <838819583@qq.com>
2024-09-02 00:06:54 +0800

    feat:add Tooltip for operation button

--- src/components/pages/HomePage.tsx
+++ src/components/pages/HomePage.tsx
@@ -1,6 +1,6 @@
 import { useOpml } from "../providers/OpmlProvider";
 import OpmlInfoList from "../common/OpmlInfoList";
-import { Box, IconButton, VStack } from "@chakra-ui/react";
+import { Box, IconButton, Tooltip, VStack } from "@chakra-ui/react";
 import { AddIcon } from "@chakra-ui/icons";
 import { BiExport } from "react-icons/bi";
 import { SiIpfs } from "react-icons/si";
@@ -20,28 +20,34 @@ const HomePage = () => {
       <Box justifyContent="center" alignItems="center">
         <OpmlInfoList opml={opml} />
         <VStack spacing={4} position="fixed" bottom="20px" right="20px">
-          <IconButton
-            icon={<SiIpfs />}
-            aria-label="Upload OPML to IPFS"
-            zIndex="1000"
-            onClick={() => {
-              uploadOpmlToIpfs();
-            }}
-          />
-          <IconButton
-            icon={<BiExport />}
-            aria-label="Export OPML"
-            zIndex="1000"
-            onClick={() => {
-              downloadFile(buildOpml(opml), "Ipfs-feed-box.OPML");
-            }}
-          />
-          <IconButton
-            icon={<AddIcon />}
-            aria-label="Add"
-            zIndex="1000"
-            onClick={() => setIsOpenAddOpmlListItem(true)}
-          />
+          <Tooltip label="Upload OPML to IPFS">
+            <IconButton
+              icon={<SiIpfs />}
+              aria-label="Upload OPML to IPFS"
+              zIndex="1000"
+              onClick={() => {
+                uploadOpmlToIpfs();
+              }}
+            />
+          </Tooltip>
+          <Tooltip label="Export OPML to local">
+            <IconButton
+              icon={<BiExport />}
+              aria-label="Export OPML"
+              zIndex="1000"
+              onClick={() => {
+                downloadFile(buildOpml(opml), "Ipfs-feed-box.OPML");
+              }}
+            />
+          </Tooltip>
+          <Tooltip label="Add Item">
+            <IconButton
+              icon={<AddIcon />}
+              aria-label="Add"
+              zIndex="1000"
+              onClick={() => setIsOpenAddOpmlListItem(true)}
+            />
+          </Tooltip>
         </VStack>
         <AddOpmlListItemModal
           isOpen={isOpenAddOpmlListItem}
