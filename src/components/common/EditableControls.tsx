ca29e16d87fb8d4508b2bcde2e64aa77f51aa94f
LinSoap <838819583@qq.com>
2024-09-17 14:52:00 +0800

    chore: change EditableControls button

--- src/components/common/EditableControls.tsx
+++ src/components/common/EditableControls.tsx
@@ -10,11 +10,19 @@ export const EditableControls = () => {
       <ButtonGroup justifyContent="center" size="sm">
         <IconButton
           icon={<CheckIcon />}
+          backgroundColor="#f4f4f4"
+          borderRadius={"12px"}
+          boxShadow={"2px 2px 0px 0px black"}
+          border={"2px solid"}
           {...getSubmitButtonProps()}
           aria-label="Save"
         />
         <IconButton
           icon={<CloseIcon />}
+          backgroundColor="#f4f4f4"
+          borderRadius={"12px"}
+          boxShadow={"2px 2px 0px 0px black"}
+          border={"2px solid"}
           {...getCancelButtonProps()}
           aria-label="Cancel"
         />
